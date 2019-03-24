4a47340972175ab91aafcece5b6c776e432540f2
Khrapun <pavelhrapun@mail.ru>
2019-03-24 19:28:50 +0300

    feat: zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zeros = 0;
+  let step = 0;
+  let osn = 0;
+  if (base == 0) {
+    osn = base;
+    step = 1;
+    zeros = 0;
+    let z = Math.floor(number/base);
+    return z;
+  }
+  else{
+    let j = 1;
+    let i = 2;
+    let a =[0];
+    do
+    {
+      if (base % i == 0) 
+      {
+        a[j] = i;
+        j++;
+        base = base / i;
+      }
+      else
+      {
+        i++;
+      }
+    }
+    while (i < base);
+    a[j] = i; 
+    osn = (a[a.length - 1]);
+    for(let c = 0; c != a.length; c++){
+      if(osn == a[c]){
+        step++;
+      }
+    }    
+  }
+  for(let step2 = 1; Math.trunc(number / Math.pow(osn, step2)) > 1; step2++) {
+    zeros += Math.trunc(number / Math.pow(osn, step2));
+  }
+  let result = Math.trunc(zeros / step)
+  return result;
 }
